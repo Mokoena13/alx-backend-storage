fdad6dc3e7a52e61a90e26389e08ea46dc73865f
Mokoena13 <ernstmokoena.mokoena@gmail.com>
2024-03-21 12:13:14 -0700

    upd

--- 0x00-MySQL_Advanced/100-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -1,15 +1,34 @@
--- creates a function SafeDiv that divides (and returns) the first
--- by the second number or returns 0 if the second number is equal to 0.
-DROP FUNCTION IF EXISTS SafeDiv;
+-- creates a stored procedure ComputeAverageWeightedScoreForUser that
+-- computes and store the average weighted score for a student.
+DROP PROCEDURE IF EXISTS ComputeAverageWeightedScoreForUser;
 DELIMITER $$
-CREATE FUNCTION SafeDiv (a INT, b INT)
-RETURNS FLOAT DETERMINISTIC
+CREATE PROCEDURE ComputeAverageWeightedScoreForUser (user_id INT)
 BEGIN
-    DECLARE result FLOAT DEFAULT 0;
+    DECLARE total_weighted_score INT DEFAULT 0;
+    DECLARE total_weight INT DEFAULT 0;
 
-    IF b != 0 THEN
-        SET result = a / b;
+    SELECT SUM(corrections.score * projects.weight)
+        INTO total_weighted_score
+        FROM corrections
+            INNER JOIN projects
+                ON corrections.project_id = projects.id
+        WHERE corrections.user_id = user_id;
+
+    SELECT SUM(projects.weight)
+        INTO total_weight
+        FROM corrections
+            INNER JOIN projects
+                ON corrections.project_id = projects.id
+        WHERE corrections.user_id = user_id;
+
+    IF total_weight = 0 THEN
+        UPDATE users
+            SET users.average_score = 0
+            WHERE users.id = user_id;
+    ELSE
+        UPDATE users
+            SET users.average_score = total_weighted_score / total_weight
+            WHERE users.id = user_id;
     END IF;
-    RETURN result;
 END $$
 DELIMITER ;
